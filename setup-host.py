fad7728aca1b3202589ee941856624a36603b059
Martijn Jacobs <martijn@devopsconsulting.nl>
2014-07-09 13:28:53 +0200

    use dmidecode to find the uuid

--- setup-host.py
+++ setup-host.py
@@ -1,5 +1,16 @@
 import base64
 import os
+import subprocess
+
+uuid = subprocess.check_output(["/usr/sbin/dmidecode", "-s", "system-uuid"]).strip()
+with open("/etc/UUID.conf", "wb") as f:
+    f.write(uuid)
+os.rename("/etc/hosts", "/etc/hosts.bak")
+with open("/etc/hosts", "wb") as f:
+    f.write("127.0.0.1    localhost\n127.0.1.1    %(uuid)s.local %(uuid)s\n" % locals())
+with open("/etc/hostname", "wb") as f:
+    f.write("%(uuid)s.local" % locals())
+
 with open("/proc/cmdline", "r") as f:
     cmdline = f.read()
     cmdline = cmdline.strip()
@@ -7,14 +18,5 @@ with open("/proc/cmdline", "r") as f:
     userdata = base64.urlsafe_b64decode(userdata + '=' * (4 - len(userdata) % 4)).split("\n")
 for x in userdata:
     key, value = x.split("=")
-    if key == 'uuid':
-        uuid = value
-        with open("/etc/UUID.conf", "wb") as f:
-            f.write(uuid)
-        os.rename("/etc/hosts", "/etc/hosts.bak")
-        with open("/etc/hosts", "wb") as f:
-            f.write("127.0.0.1    localhost\n127.0.1.1    %(uuid)s.local %(uuid)s\n" % locals())
-        with open("/etc/hostname", "wb") as f:
-            f.write("%(uuid)s.local" % locals())
     with open("/etc/environment", "a") as f:
         f.write("FACTER_%s=%s\n" % (key, value))
